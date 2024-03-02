d052422a97a6ec829f3eea4c6df5d099d565a0ee
dave-birri <mcdivitbirri@gmail.com>
2024-03-03 00:20:57 +0200

    prep 2 hw

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -3,7 +3,44 @@ using System;
 class Program
 {
     static void Main(string[] args)
+
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string UserInput = Console.ReadLine();
+        int percent = int.Parse(UserInput);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed, Keep it Up!");
+        }
+        else
+        {
+            Console.WriteLine("Don't be dicouraged, you can do it next time!");
+        }
     }
 }
