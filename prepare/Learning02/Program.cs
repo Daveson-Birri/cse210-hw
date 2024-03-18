7d0528c6b8a9ac7fa58f459bfd4e8940a8fb7237
dave-birri <mcdivitbirri@gmail.com>
2024-03-18 13:36:13 +0200

    lesson

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "General Manager";
+        job1._company = "Piza E Vino Irene";
+        job1._startYear = 2019;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Sales Manager";
+        job2._company = "The Wild Peach";
+        job2._startYear = 2024;
+        job2._endYear = 0000;
+
+        Resume myResume = new Resume();
+        myResume._name = "Daveson McDivit Birri";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
